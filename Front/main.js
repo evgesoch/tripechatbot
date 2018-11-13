812d73692e181f88d716b65bdc38a5812c9a26ae
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2018-11-13 14:39:41 +0200

    fixes

--- Front/main.js
+++ Front/main.js
@@ -7,13 +7,13 @@ function sendmsg(){
     alert(str1);
 }
 
-function buildMessage(){
+function buildMessage(msg){
 
     var newMessage =    '<div class="row">' + 
                             '<div class="col-sm-6"></div>' +
                             '<div class="col-sm-6">' +
                                 '<div id="tb-testimonial" class="testimonial testimonial-success-filled">' + 
-                                    '<div class="testimonial-section">Interested</div>' +
+                                    '<div class="testimonial-section">' + msg + '</div>' +
                                     '<div class="testimonial-desc">' + 
                                         '<img src="user.png" alt="user avatar" />' +
                                         '<div class="testimonial-writer">' + 
