7211d38d03bdf0ca755a3eaece16ea579cc21432
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2018-11-13 14:29:57 +0200

    Enabled interested button functionality

--- Front/main.js
+++ Front/main.js
@@ -6,3 +6,23 @@ function sendmsg(){
     str1= document.getElementById("myText").value;
     alert(str1);
 }
+
+function buildMessage(){
+
+    var newMessage =    '<div class="row">' + 
+                            '<div class="col-sm-6"></div>' +
+                            '<div class="col-sm-6">' +
+                                '<div id="tb-testimonial" class="testimonial testimonial-success-filled">' + 
+                                    '<div class="testimonial-section">Interested</div>' +
+                                    '<div class="testimonial-desc">' + 
+                                        '<img src="user.png" alt="user avatar" />' +
+                                        '<div class="testimonial-writer">' + 
+                                            '<div class="testimonial-writer-name">User</div>' +
+                                        '</div>' +
+                                    '</div>' + 
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+
+    $("#msg-container").append(newMessage);
+}
