f4f34e995d8686a6b1efbc55fbd5e2047a6f680c
georgegant <44404922+georgegant@users.noreply.github.com>
2018-11-27 17:02:24 +0200

    z6z56zz6z

--- Front/main.js
+++ Front/main.js
@@ -1,5 +1,5 @@
 var message;
-//var nbEl = 1;
+var nbEl = 1;
 
 function sendmsg(){
     
@@ -27,13 +27,25 @@ if(message=="thessaloniki"){
                 '</div>';
     $("#msg-container").append(usermessage);
 
-   /* nbEl++;
+    nbEl++;
     var divClone = $("#element_1").clone();
     divClone.attr("id","element_"+nbEl)
     divClone.appendTo("body").show();
-    $("#msg-container").append(divClone);*/
-
-
+    $("#msg-container").append(divClone);
+}else{
+    var errormsg =   '<div class="col-sm-6">'+
+    '<div id="tb-testimonial" class="testimonial testimonial-default-filled ">'+
+        '<div   class="testimonial-section">'+
+        '<p>Sorry! I can not understand you. Please type again! </p>'+
+        '</div>'+
+        '<div class="testimonial-desc">'+
+            '<img src="bot.jpg" alt="" />'+
+            '<div class="testimonial-writer">'+
+                '<div class="testimonial-writer-name">Frank</div>'  + 
+            '</div>'+
+        '</div>'+
+    '</div>';
+    $("#msg-container").append(errormsg);
 }
     
 }
